2f289244cd0d3d5bb9ba35a4c4c8a2f142582bf3
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-03 11:52:52 +0530

    Fix register error handling

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,12 +1,19 @@
 const User = require('../model/User')
 const bcrypt = require('bcrypt');
 
-const register = async (req,res)=>{
+const register = async (req, res) => {
   try {
-    const {fullName, email, username, password, picturePath} = req.body
+    const { fullName, email, username, confirmPassword, password, picturePath } = req.body
 
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
+
+    if (!fullName || !username || !email || !password || !confirmPassword) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
 
     const newUser = await User.create({
       fullName,
@@ -18,9 +25,18 @@ const register = async (req,res)=>{
 
     res.status(201).json(newUser)
 
+
   } catch (err) {
-    res.status(500).json({message: err.message})
+    res.status(500).json({ message: err.message })
   }
 }
 
-module.exports = {register}
+const login = async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
+
+module.exports = { register }
