8c7882dba0fa5e08cbe11bbe5315b8ec76c2686d
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-03 13:46:31 +0530

    Add file upload route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const authRouter = require('./auth.route')
+const uploadRouter = require('./upload.route')
 const userRouter = require('./user.route')
 
 const api = express.Router()
 
 api.use('/auth', authRouter)
 api.use('/user', userRouter)
+api.use('/upload', uploadRouter)
 
 module.exports = api
