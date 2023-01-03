282bc27192931807cc1d0fd9e89cec72f5623b3e
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-03 12:33:00 +0530

    Add login route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require('../model/User')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   try {
@@ -33,10 +34,21 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
+    const {username, email, password} = req.body
+   
+    const user = await User.findOne({ $or: [{ email }, { username }] })
 
+    if(!user) return res.status(404).json({ message: 'User does not exist' })
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) return res.status(403).json({ message: 'Invalid credentials'})
+
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+    delete user.password
+    res.status(200).json({ token , user})
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
 }
 
-module.exports = { register }
+module.exports = { register, login }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,12 +1,12 @@
 const express = require('express')
 // import { upload } from '../../services/file-storage.js'
 // import { login, register } from '../controllers/auth.controller.js'
-const {register} = require('../controllers/auth.controller')
+const {register, login} = require('../controllers/auth.controller')
 
 const authRouter = express.Router()
 
 authRouter.post('/register', register)
-// authRouter.post('/login', login)
+authRouter.post('/login', login)
 
 // export default authRouter
 module.exports = authRouter
