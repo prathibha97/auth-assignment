efd01017f7cf194fc97f7c5694e21599128aa356
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-03 11:01:43 +0530

    Add DB connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 require('dotenv').config()
 const http = require('http')
 const app = require('./app')
+const connectDb = require('./services/db')
 
 const server = http.createServer(app)
 const port = process.env.PORT || 5001
 
-const startServer = () =>{
+const startServer = async () => {
 
-  server.listen(port, ()=>{
-      console.log(`Server listening on port ${port}`)
+  await connectDb()
+
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
   })
 }
 
