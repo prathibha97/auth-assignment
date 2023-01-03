27582b8edf11a4f31929ae9f9922e8ab5a6176a6
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-03 11:53:12 +0530

    Update user schema

--- server/model/User.js
+++ server/model/User.js
@@ -8,10 +8,12 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
